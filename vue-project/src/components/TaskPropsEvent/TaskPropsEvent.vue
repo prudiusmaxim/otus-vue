f185af78aee8f0064e1e41ad9032e92efa2c6aec
esurgu <esurgu@mail.ru>
2025-08-29 01:24:21 +0300

    dev

--- vue-project/src/components/TaskPropsEvent/TaskPropsEvent.vue
+++ vue-project/src/components/TaskPropsEvent/TaskPropsEvent.vue
@@ -11,7 +11,6 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { productsMock } from './mock-data'
 import type { Product } from './types/Product'
 import ProductComponent from './ProductComponent.vue';
 
@@ -19,12 +18,12 @@ const products = ref<Product[]>([]);
 
 const fetchProducts = async () => {
 
-    const response = {
+    /*const response = {
         ok: true,
         json: async () => productsMock
-     } as Response; 
+     } as Response; */
 
-    //const response = await fetch('https://fakestoreapi.com/products');
+    const response = await fetch('https://fakestoreapi.com/products');
 
     try {
         if (!response.ok) {
